3f9186910430a012d3ecfc2d7c9c772ded1d320f
Luisxz24 <lugeju-9062005@hotmail.com>
2026-05-19 11:16:08 -0600

    doc para data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Polygons of AGEB from Coatepec 
+#'
+#' A spatial dataset containing of polygon data of agebs from Coatepec
+#'
+#' @format A sf object with 18 rows and 2 variables:
+#' \describe{
+#'   \item{CVEGEO}{the ids.}
+#'   \item{geometry}{simple feature.}
+#'   ...
+#' }
+#' @source \url{https://pautas.ine.mx/transparencia/mapas/}
+#' @details This datasets were published in 2025.
+"ageb"
